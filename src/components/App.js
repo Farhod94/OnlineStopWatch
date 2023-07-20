fcd249ef7905be3a0398aaf7347672b637ff10ba
axmatovfarhod <axmatov.farxod.94@mail.ru>
2023-07-20 22:44:56 +0500

    stopwatch project updates

--- src/components/App.js
+++ src/components/App.js
@@ -5,33 +5,99 @@ class App extends Component{
     constructor(props){
         super(props);
         this.state = {
-            myCount: 0,
+            hour: 0,
+            minut: 0,
+            secund: 0,
+            interval: "",
+            btnBool: false,
+            intervalArray: [],
         }
     }
-    oneAdd = () =>{
+    
+    startTime = () =>{
+        let timer = setInterval(() => {
+            if (this.state.secund === 59) {
+                if (this.state.minut === 59) {
+                    if (this.state.hour === 59) {
+                        this.setState({
+                            hour: 0,
+                            minut: 0,
+                            secund: 0,
+                        });
+                    }else{
+                        this.setState({
+                            hour: this.state.hour + 1,
+                            minut: 0,
+                            secund: 0,
+                        });
+                    }
+                }else{
+                    this.setState({
+                        minut: this.state.minut + 1,
+                        secund: 0,
+                    });
+                }    
+            } else{
+                this.setState({secund: this.state.secund + 1}) 
+            }
+        },1000);
         this.setState({
-            myCount: this.state.myCount + 1
+            btnBool: true,
         });
-        
+        this.setState({
+            interval: timer,
+        });
     };
-    deleteOne = () =>{
+    stopTime = () =>{
+        clearInterval(this.state.interval);
         this.setState({
-            myCount: this.state.myCount - 1
+            btnBool: false,
         });
-        
     };
-    resetFunc = () =>{
+    myInterval = () =>{
+        const {hour, minut, secund, intervalArray} = this.state;
+        intervalArray.push(`${hour}:${minut}:${secund}`);
         this.setState({
-            myCount: 0
+            intervalArray,
         });
-        
+    };
+    clearFunc = () =>{
+        this.stopTime();
+        this.setState({
+            hour: 0,
+            minut: 0,
+            secund: 0,
+            intervalArray: [],
+        });
     };
     render(){
+        const {hour, minut, secund} = this.state;
         return <div>
-            <h1>Counter: {this.state.myCount}</h1>        
-            <button onClick={this.myFunc}>Counter ++1</button>
-            <button onClick={this.myFunc}>Counter --1</button>
-            <button onClick={this.myFunc}>Reset</button>
+            <div className="container">
+                
+                    <div className="header">
+                        <h1>Online </h1>
+                        <h2>stopwatch</h2>
+                    </div>
+                
+                <div className="child-time-con">
+                    <h1>{hour}</h1>
+                    <p>:</p>
+                    <h1>{minut}</h1>
+                    <sub>{secund}</sub>
+                </div>
+                <div className="child-btn-con">
+                    <button id="one" onClick={this.startTime} disabled={this.state.btnBool}>Start</button>
+                    <button id="two" onClick={this.stopTime}>Stop</button>
+                    <button id="three" onClick={this.myInterval}>Interval</button>
+                    <button id="four" onClick={this.clearFunc}>Clear</button>
+                </div>
+                <div className="child-list-con">
+                    {this.state.intervalArray.map(i => <p>{i}</p>)}
+                </div>
+            </div>
+            
+            
         </div>
     }
 }
